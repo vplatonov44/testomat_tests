3f4807eadfcbe70daa76504cd44630c69585321b
vplatonov <ceopltn@gmail.com>
2025-12-17 22:39:47 +0200

    Add initial setup for Playwright tests

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,8 +1,21 @@
-import unittest
+from playwright.sync_api import Page, expect
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+def login_with_invalid_creds(page: Page):
+    page.goto("https://testomat.io")
+
+
+    expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
+
+    expect(page.get_by_text("Log in", exact=True)).to_be_visible()
+
+    page.get_by_text("Log in", exact=True).click()
+
+    #page.get_by_role("textbox", name="name@email.com")
+    page.locator("#content-desktop #user_email").fill("ceopltn@gmail.com")
+    page.locator("#content-desktop #user_password").fill("asddad")
+    page.get_by_role("button", name="Sign in").click( )
+
+
+    expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
+    expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
